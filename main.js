1ff4f1918b0719be985b8ce96af9ab8fb1f58a11
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-05-12 17:35:59 +0100

    Artificial sensors

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const canvas = document.getElementById("myCanvas");
-canvas.height = window.innerHeight;
-canvas.width = 200;
+canvas.width=200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
@@ -9,7 +8,7 @@ const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 animate();
 
 function animate() {
-  car.update();
+  car.update(road.borders);
 
   canvas.height = window.innerHeight;
 
